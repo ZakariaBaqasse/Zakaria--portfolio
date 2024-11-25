b94874cddf2de5c5a36485a860881f9ef8671a6a
ZakThink <zak@thinkable.co>
2024-11-25 20:42:27 +0100

    fix: fixed accessibility and performance issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
 import AnimatedTitle from "@/lib/components/shared/AnimatedTitle";
 import CTAButton from "@/lib/components/shared/CTAButton";
-import TransitionEffect from "@/lib/components/shared/TransitionEffect";
 import { ExternalLink } from "lucide-react";
 import Image from "next/image";
 export default async function Home() {
   return (
     <>
-      <TransitionEffect />
       <main className="flex min-h-screen items-center text-dark dark:text-light sm:items-start">
         <div
           className="z-0 inline-block h-full w-full bg-light dark:bg-dark xl:p-24 lg:p-16 
@@ -30,7 +28,7 @@ export default async function Home() {
                   className="lg:text-left text-center"
                   text="Building Smarter Solutions with Code and AI Innovation."
                 />
-                <p className="md:text-lg text-sm lg:text-left text-center">
+                <p className="md:text-lg text-sm lg:text-left text-center text-dark">
                   As a skilled software and AI engineer, I specialize in
                   transforming ideas into intelligent, scalable solutions.
                   Explore my latest projects and articles, highlighting my
@@ -38,13 +36,20 @@ export default async function Home() {
                   technologies
                 </p>
                 <div className="flex flex-wrap gap-4 w-full justify-center lg:justify-start">
-                  <Button className="gap-2 bg-dark">
+                  <Button
+                    name="download-resume button"
+                    className="gap-2 px-4 py-2 w-60 h-12 bg-dark text-base"
+                  >
                     <a href="/files/resume.pdf" target="_blank">
                       Resume
                     </a>
                     <ExternalLink className="h-4 w-4" />
                   </Button>
-                  <Button variant="outline">
+                  <Button
+                    name="contact via email button"
+                    variant="outline"
+                    className="px-4 py-2 w-60 h-12 text-base"
+                  >
                     <a href="mailto:zakaria.baqasse@gmail.com">Contact</a>
                   </Button>
                 </div>
